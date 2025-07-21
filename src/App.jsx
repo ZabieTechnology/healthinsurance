d88f42d1475c9a66c796993a37a60cf0e10c8aa3
ZabieTechnology <development@zabietechnology.com>
2025-07-21 15:59:56 +0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Medical Insurance Portal</h1>
+      <p>Application is running!</p>
+    </div>
+  )
+}
+
+export default App
